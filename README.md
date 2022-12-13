24c3dbe0c57c02b33b976daef0e6f97122730133
JubelHossain <jubel_hossain@appnap.io>
2022-12-13 11:46:04 +0600

    initial project set up

--- README.md
+++
@@ -1,2 +0,0 @@
-# TextToImage_StableDiffusionV2
-This is a simple project which can convert the user text to artwork using Stable diffusionV2 
---
+++ TextToImageSD2/TextToImageSD2App.swift
@@ -0,0 +1,17 @@
+//
+//  TextToImageSD2App.swift
+//  TextToImageSD2
+//
+//  Created by Appnap WS13 on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct TextToImageSD2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
